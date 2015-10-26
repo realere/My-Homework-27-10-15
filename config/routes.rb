68e9dbbc015a5de22b81611002e61c7efef60daa
User <user@Users-MacBook-Pro-10.local>
2015-10-26 23:26:29 +0000

    working version 2 before css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#welcome'
   get '/welcome', to: 'home#welcome'
+  get '/prices', to: 'home#prices'
+  get '/booking', to: 'home#booking'
+  get '/contact', to: 'home#contact'
   get '/hair', to: 'home#hair'
   get '/makeup', to: 'home#makeup'
   get '/spa', to: 'home#spa'
